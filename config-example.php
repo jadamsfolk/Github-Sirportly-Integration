6015aa18118ce52c43953f2a4755e82aaa6b4c46
Jon Adams <jon.adams@wearefolk.com>
2014-02-26 11:51:55 +0000

    Minor change to config example script

--- config-example.php
+++ config-example.php
@@ -1,3 +1,13 @@
 <?php
-define('ROOT_DIR', 'http://xxxx');
-define('SIRPORTLY_TOKEN', 'xxxx-xxxx-xxxx-xxxx-xxxx');
+/**
+ * Please save this as config.php in the root directory
+ *
+ * Configurable Options
+ */
+define('SIRPORTLY_TOKEN', 'xxxxx-xxxxx-xxxxx-xxxxx-xxxxx');
+
+/**
+ * Non-configurable Options and Constants - Please don't touch unless you know what you're doing
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_DIR', dirname(__FILE__).DS);
